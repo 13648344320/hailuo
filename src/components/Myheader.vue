21bf0bc01fd12f1ac01146c1ea2547db6cf7afb7
LSQ <LSQ@tedu.cn>
2019-08-26 12:31:34 +0800

    ceshi

--- src/components/Myheader.vue
+++ src/components/Myheader.vue
@@ -43,7 +43,7 @@
                 // 如果找到
                 // 携带参数跳转到 list 页面
                 //  list 
-                // 李树强
+                // asfadfsadfsfsdf
                 var url="find";
                 var obj ={obj:this.find} ;
                 this.axios.get(url,{params:obj}).then(res=>{
